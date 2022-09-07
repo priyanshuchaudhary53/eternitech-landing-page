d31fa14f9df43ad35c57fcd5b2366cad1470cfe6
Priyanshu Chaudhary <85029348+priyanshuchaudhary53@users.noreply.github.com>
2022-09-08 00:21:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,50 +7,6 @@
   jQuery(document).ready(function () {
     "use strict";
 
-    // Popup Form 'Book a meeting' btn
-    $("#book-meeting-btn").click(function () {
-      event.preventDefault();
-      $("#popup-form-section-1").addClass("hidden");
-      $("#popup-form-section-2").removeClass("hidden");
-      $("#book-meeting-submit").removeClass("hidden");
-    });
-
-    // Popup Form 'Chat with a sales rep.' btn
-    $("#chat-sales-btn").click(function () {
-      event.preventDefault();
-      $("#popup-form-section-1").addClass("hidden");
-      $("#popup-form-section-2").removeClass("hidden");
-      $("#chat-sales-submit").removeClass("hidden");
-    });
-
-    // Popup Form 'Get back to me' btn
-    $("#get-back-btn").click(function () {
-      event.preventDefault();
-      $("#popup-form-section-1").addClass("hidden");
-      $("#popup-form-section-2").removeClass("hidden");
-      $("#get-back-submit").removeClass("hidden");
-    });
-
-    // Popup Form 'Back' btn
-    $("#popup-form-back").click(function () {
-      event.preventDefault();
-      $("#popup-form-section-1").removeClass("hidden");
-      $("#popup-form-section-2").addClass("hidden");
-      $("#book-meeting-submit").addClass("hidden");
-      $("#chat-sales-submit").addClass("hidden");
-      $("#get-back-submit").addClass("hidden");
-    });
-
-    // Popup close btn
-    $("#popup-close").click(function() {
-      $('#popup-form').trigger('reset');
-      $("#popup-form-section-1").removeClass("hidden");
-      $("#popup-form-section-2").addClass("hidden");
-      $("#book-meeting-submit").addClass("hidden");
-      $("#chat-sales-submit").addClass("hidden");
-      $("#get-back-submit").addClass("hidden");
-    });
-
     //   Testimonial Carousel
     $(".owl-carousel").owlCarousel({
       loop: true,
